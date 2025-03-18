de9cde955dd5c1d4df41a9478c1bb57400579db0
dylancmznus <e1121891@u.nus.edu>
2025-03-19 02:34:06 +0800

    edited checkstyle

--- src/test/java/cetest/PatientTest.java
+++ src/test/java/cetest/PatientTest.java
@@ -3,7 +3,8 @@ package cetest;
 import MainFunction.ManagementSystem;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PatientTest {
     @Test
