88ef717df13fc81885251cd05b57054f8bf70fc9
yukuan <e1249416@u.nus.edu>
2025-03-19 19:07:45 +0800

    fix the Junit test error

--- src/test/java/MedicalHistoryManagerTest.java
+++ src/test/java/MedicalHistoryManagerTest.java
@@ -2,10 +2,11 @@ import manager.Patient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class MedicalHistoryManagerTest {
 
